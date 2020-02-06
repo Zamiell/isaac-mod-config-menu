996a1dce88ecf609fa144f46e72491041a4fd64a
piber20 <piber20@cygo.com>
2020-02-06 09:49:57 -0500

    add files from apis repo

---
+++ main.lua
@@ -0,0 +1,22 @@
+modconfighousingmod = RegisterMod("mod config menu standalone", 1)
+ModConfigMenuIsStandalone = true
+
+local _, err = pcall(require, "scripts.modconfig.modconfig")
+err = tostring(err)
+if not string.match(err, "attempt to call a nil value %(field 'ForceError'%)") then
+	if string.match(err, "true") then
+		err = "Error: require passed in modconfig"
+	end
+	error(err)
+end
+
+ModConfigMenuIsStandalone = nil
+if ModConfigMenu then
+	ModConfigMenu.StandaloneMod = true
+
+	if ModConfigMenu.FinishedLoadingMessage then
+		ModConfigMenu.FinishedLoadingMessage = "Mod Config Menu v" .. ModConfigMenu.Version .. " Standalone loaded!"
+		Isaac.DebugString(ModConfigMenu.FinishedLoadingMessage)
+		print(ModConfigMenu.FinishedLoadingMessage)
+	end
+end
