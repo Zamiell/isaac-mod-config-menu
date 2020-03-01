bb54ce2161cfae424984ebe188c37e94d1f71a0f
piber20 <piber20@cygo.com>
2020-03-01 16:13:16 -0500

    move compatibility to main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,11 @@
 local mod = RegisterMod("Mod Config Menu Standalone", 1)
 local MCM = require("scripts.modconfig")
 
+
+----------
+--SAVING--
+----------
+
 local game = Game()
 local level = game:GetLevel()
 
@@ -52,5 +57,42 @@ mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, function(_, shouldSave)
 	
 end)
 
---some handling to let old mods work
-require("scripts.modconfigoldcompat")
+
+-------------------------
+--OLD MOD COMPATIBILITY--
+-------------------------
+
+ModConfigMenu = {}
+ModConfigMenu.Version = 13
+
+ModConfigMenu.Config = MCM.Config
+
+ModConfigMenuPopupGfx = MCM.PopupGfx
+ModConfigMenuOptionType = MCM.OptionType
+
+ModConfigMenu.SetOldCategoryInfo = function(category)
+	MCM.UpdateCategory(category, {
+		Info = {
+			"This mod adds settings through",
+			"an older method and may not work right"
+		},
+		IsOld = true
+	})
+end
+
+ModConfigMenu.AddSetting = function(category, subcategory, settingTable)
+	ModConfigMenu.SetOldCategoryInfo(category)
+	return MCM.AddSetting(category, subcategory, settingTable)
+end
+ModConfigMenu.AddText = function(category, subcategory, text, color)
+	ModConfigMenu.SetOldCategoryInfo(category)
+	return MCM.AddText(category, subcategory, text, color)
+end
+ModConfigMenu.AddTitle = function(category, subcategory, text, color)
+	ModConfigMenu.SetOldCategoryInfo(category)
+	return MCM.AddTitle(category, subcategory, text, color)
+end
+ModConfigMenu.AddSpace = function(category, subcategory)
+	ModConfigMenu.SetOldCategoryInfo(category)
+	return MCM.AddSpace(category, subcategory)
+end
