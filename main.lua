7825075f51b607e27f21c52bc21508feff1e84b5
piber20 <piber20@gmail.com>
2020-04-04 01:12:11 -0400

    restore ModConfigMenuData for old mods

--- main.lua
+++ main.lua
@@ -7,6 +7,7 @@ local mod = RegisterMod("Mod Config Menu Standalone", 1)
 local MCM = require("scripts.modconfig")
 local CallbackHelper = require("scripts.callbackhelper")
 local SaveHelper = require("scripts.savehelper")
+local CacheHelper = require("scripts.cachehelper")
 
 
 ----------
@@ -39,10 +40,37 @@ end, mod)
 ModConfigMenu = {}
 ModConfigMenu.Version = 13
 
+ModConfigMenu.VECTOR_ZERO = Vector(0,0)
+ModConfigMenu.VECTOR_ONE = Vector(1,1)
+
+ModConfigMenu.VECTOR_LEFT = Vector(-1,0)
+ModConfigMenu.VECTOR_UP = Vector(0,-1)
+ModConfigMenu.VECTOR_RIGHT = Vector(1,0)
+ModConfigMenu.VECTOR_DOWN = Vector(0,1)
+ModConfigMenu.VECTOR_UP_LEFT = Vector(-1,-1)
+ModConfigMenu.VECTOR_UP_RIGHT = Vector(1,-1)
+ModConfigMenu.VECTOR_DOWN_LEFT = Vector(-1,1)
+ModConfigMenu.VECTOR_DOWN_RIGHT = ModConfigMenu.VECTOR_ONE
+
+ModConfigMenu.COLOR_DEFAULT = Color(1,1,1,1,0,0,0)
+ModConfigMenu.COLOR_HALF = Color(1,1,1,0.5,0,0,0)
+ModConfigMenu.COLOR_INVISIBLE = Color(1,1,1,0,0,0,0)
+ModConfigMenu.KCOLOR_DEFAULT = KColor(1,1,1,1)
+ModConfigMenu.KCOLOR_HALF = KColor(1,1,1,0.5)
+ModConfigMenu.KCOLOR_INVISIBLE = KColor(1,1,1,0)
+
+ModConfigMenu.Game = CacheHelper.Game
+ModConfigMenu.Seeds = CacheHelper.Seeds
+ModConfigMenu.Level = CacheHelper.Level
+ModConfigMenu.Room = CacheHelper.Room
+ModConfigMenu.SFX = CacheHelper.SFX
+
 ModConfigMenu.Config = MCM.Config
 
 ModConfigMenuPopupGfx = MCM.PopupGfx
 ModConfigMenuOptionType = MCM.OptionType
+
+ModConfigMenuData = MCM.MenuData
 
 ModConfigMenu.SetOldCategoryInfo = function(category)
 	MCM.UpdateCategory(category, {
@@ -83,29 +111,3 @@ end
 ModConfigMenu.AddBigBookChangeCallback = function(functionToAdd)
 	CallbackHelper.AddCallback(mod, CallbackHelper.Callbacks.MCM_POST_MODIFY_BIG_BOOKS, functionToAdd)
 end
-
-ModConfigMenu.VECTOR_ZERO = Vector(0,0)
-ModConfigMenu.VECTOR_ONE = Vector(1,1)
-
-ModConfigMenu.VECTOR_LEFT = Vector(-1,0)
-ModConfigMenu.VECTOR_UP = Vector(0,-1)
-ModConfigMenu.VECTOR_RIGHT = Vector(1,0)
-ModConfigMenu.VECTOR_DOWN = Vector(0,1)
-ModConfigMenu.VECTOR_UP_LEFT = Vector(-1,-1)
-ModConfigMenu.VECTOR_UP_RIGHT = Vector(1,-1)
-ModConfigMenu.VECTOR_DOWN_LEFT = Vector(-1,1)
-ModConfigMenu.VECTOR_DOWN_RIGHT = ModConfigMenu.VECTOR_ONE
-
-ModConfigMenu.COLOR_DEFAULT = Color(1,1,1,1,0,0,0)
-ModConfigMenu.COLOR_HALF = Color(1,1,1,0.5,0,0,0)
-ModConfigMenu.COLOR_INVISIBLE = Color(1,1,1,0,0,0,0)
-ModConfigMenu.KCOLOR_DEFAULT = KColor(1,1,1,1)
-ModConfigMenu.KCOLOR_HALF = KColor(1,1,1,0.5)
-ModConfigMenu.KCOLOR_INVISIBLE = KColor(1,1,1,0)
-
-local CacheHelper = require("scripts.cachehelper")
-ModConfigMenu.Game = CacheHelper.Game
-ModConfigMenu.Seeds = CacheHelper.Seeds
-ModConfigMenu.Level = CacheHelper.Level
-ModConfigMenu.Room = CacheHelper.Room
-ModConfigMenu.SFX = CacheHelper.SFX
