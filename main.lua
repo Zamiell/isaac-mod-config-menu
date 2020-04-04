792febf130d5080a075468c94da88dd6d16cd3c8
piber20 <piber20@gmail.com>
2020-04-03 23:37:07 -0400

    add some cached values for old mods

--- main.lua
+++ main.lua
@@ -83,3 +83,29 @@ end
 ModConfigMenu.AddBigBookChangeCallback = function(functionToAdd)
 	CallbackHelper.AddCallback(mod, CallbackHelper.Callbacks.MCM_POST_MODIFY_BIG_BOOKS, functionToAdd)
 end
+
+ModConfigMenu.VECTOR_ZERO = Vector(0,0)
+ModConfigMenu.VECTOR_ONE = Vector(1,1)
+
+ModConfigMenu.VECTOR_LEFT = Vector(-1,0)
+ModConfigMenu.VECTOR_UP = Vector(0,-1)
+ModConfigMenu.VECTOR_RIGHT = Vector(1,0)
+ModConfigMenu.VECTOR_DOWN = Vector(0,1)
+ModConfigMenu.VECTOR_UP_LEFT = Vector(-1,-1)
+ModConfigMenu.VECTOR_UP_RIGHT = Vector(1,-1)
+ModConfigMenu.VECTOR_DOWN_LEFT = Vector(-1,1)
+ModConfigMenu.VECTOR_DOWN_RIGHT = ModConfigMenu.VECTOR_ONE
+
+ModConfigMenu.COLOR_DEFAULT = Color(1,1,1,1,0,0,0)
+ModConfigMenu.COLOR_HALF = Color(1,1,1,0.5,0,0,0)
+ModConfigMenu.COLOR_INVISIBLE = Color(1,1,1,0,0,0,0)
+ModConfigMenu.KCOLOR_DEFAULT = KColor(1,1,1,1)
+ModConfigMenu.KCOLOR_HALF = KColor(1,1,1,0.5)
+ModConfigMenu.KCOLOR_INVISIBLE = KColor(1,1,1,0)
+
+local CacheHelper = require("scripts.cachehelper")
+ModConfigMenu.Game = CacheHelper.Game
+ModConfigMenu.Seeds = CacheHelper.Seeds
+ModConfigMenu.Level = CacheHelper.Level
+ModConfigMenu.Room = CacheHelper.Room
+ModConfigMenu.SFX = CacheHelper.SFX
