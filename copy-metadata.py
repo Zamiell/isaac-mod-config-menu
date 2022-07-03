bf7e0070348d8f98fed265e5eb96b3b01ec9bd86
James <5511220+Zamiell@users.noreply.github.com>
2022-07-02 20:29:13 -0400

    release

---
+++ copy-metadata.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+SCRIPT_PATH = os.path.realpath(__file__)
+SOURCE_MOD_DIRECTORY = os.path.dirname(SCRIPT_PATH)
+TARGET_MOD_DIRECTORY = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\The Binding of Isaac Rebirth\\mods\\!!mod config menu"
+
+METADATA_XML = "metadata.xml"
+SOURCE_METADATA_XML = os.path.join(SOURCE_MOD_DIRECTORY, METADATA_XML)
+TARGET_METADATA_XML = os.path.join(TARGET_MOD_DIRECTORY, METADATA_XML)
+
+# Copy back the "metadata.xml" file, since it will have an incremented version number.
+shutil.copyfile(TARGET_METADATA_XML, SOURCE_METADATA_XML)
