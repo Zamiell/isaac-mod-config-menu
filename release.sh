519952858c4aa45d74b4819dce1d3f2196e206a1
James <5511220+Zamiell@users.noreply.github.com>
2023-02-04 13:45:31 -0500

    chore: better release script

--- release.sh
+++ release.sh
@@ -8,4 +8,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
 
+git pull --rebase
+
+if ! npx git-dirty; then
+  # The current working directory is dirty. (Unintuitively, the "git-dirty" returns 1 if the current
+  # working directory is dirty.)
+  echo "Error: The current working directory must be clean before releasing a new version. Please push your changes to Git."
+  exit 1
+fi
+
 python "$DIR/release.py"
+
+git add --all && git commit --message "chore: release" && git push
