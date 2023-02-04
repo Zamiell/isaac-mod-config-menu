0fd674f122a57db00242ce9af4863a12e1593b79
James <5511220+Zamiell@users.noreply.github.com>
2023-02-04 13:42:56 -0500

    fix: typo

--- scripts/modconfig.lua
+++ scripts/modconfig.lua
@@ -7,7 +7,7 @@ local json = require("json")
 
 -- The final version of Chifilly's Mod Config Menu fork was 33.
 -- For the pure version, we arbitrarily selected a starting point of 100 and incremented from there.
-local VERSION = 107
+local VERSION = 108
 local IS_DEV = false
 
 ModConfigMenu = {}
